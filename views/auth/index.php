8a2921f3390da9db2dfdc1de69eefd20348ffb71
ravoffc <ravimuhp907@gmail.com>
2023-03-14 09:30:13 +0700

    first commit

---
+++ views/auth/index.php
@@ -0,0 +1,24 @@
+<div class ="card mt-5" style="width: 30rem; margin: auto;">
+    <div class="card-body">
+        <h1 class="card-header">Login</h1>
+        <?php if( $this->session->flashdata('message')) :?>
+            <div class=" alert alert-warnig alert-dismissible fade show mt-2 mb-2" role="alert">
+                <strong><?= $this->session->flashdata('message'); ?></strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
+            </div>
+        <?php endif; ?>
+        <form  method="post" action="<?= base_url('auth/index');?>">
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username" name="username">
+                <?= form_error('username', '<small class="text-danger pl-3">', '</small>');?>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+                <?= form_error('password', '<small class="text-danger pl-3">', '</small>');?>
+            </div>
+            <input type="submit" value="submit" class="btn btn-primary"></input>
+        </form>
+    </div>
+</div>
